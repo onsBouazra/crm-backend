6bdc5eee006ae0082ed594e93b30e7159cdcf4d6
ons.bouazra <mailto:ons.bouazra@continuousnet.com>
2022-08-14 19:00:21 +0100

    [INIT]project init

---
+++ src/main/java/com/example/test/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World " + LocalDateTime.now();
+    }
+
+}
